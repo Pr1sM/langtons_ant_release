419c05ecdaf28765c555101699f6927a33ff1e11
Srinivas Dhanwada <dhanwads@gmail.com>
2016-08-25 17:56:02 -0500

    Added comments Changed encoding to render every 5th frame Changed implementation of move_ant_forward to move correctly Consolidated all rotate_ant functions to one

--- dhanwada_srinivas.assignment-0.c
+++ dhanwada_srinivas.assignment-0.c
@@ -115,34 +115,43 @@ typedef struct {
 } Ant;
 
 Ant* init_ant() {
+    // allocate memory for the ant and set the
+    // current_postition and direction to the
+    // correct default values.
     Ant* newAnt = (Ant*) malloc(sizeof(Ant));
     newAnt->current_position.x = _x_size / 2;
     newAnt->current_position.y = _y_size / 2;
-    newAnt->direction = 2;
+    newAnt->direction = 0;
     return newAnt;
 }
 
-void _rotate_ant(Ant* ant, int num_times, int dir) {
+void rotate_ant(Ant* ant, int num_times, int dir) {
+    // NOTE: we are asserting that dir will be either 1 or -1 based on if the intended
+    //       direction of rotation is clockwise or counterclockwise.
     int i;
     for(i = 0; i < num_times; i++) {
+        // increment the direction, then correct if there is overflow.
         ant->direction += dir;
         ant->direction = ant->direction < 0 ? 3 : ant->direction > 3 ? 0 : ant->direction;
     }
 }
 
-void rotate_ant_cw(Ant* ant, int num_times) {
-    _rotate_ant(ant, num_times, 1);
-}
-
-void rotate_ant_ccw(Ant* ant, int num_times) {
-    _rotate_ant(ant, num_times, -1);
-}
-
 void move_ant_forward(Ant* ant) {
+    // The cardinal directions N, E, S, W are represented with values 0, 1, 2, 3, respectively.
+    // That means a value of 0 or 2 affects the y direction, and 1 or 3 affects the x direction.
+    // Check which axis we need to change first.
     if(ant->direction % 2) {
-        ant->current_position.x -= ant->direction - 2;
+        // direction is either E or W (1 or 3).
+        // E changes the x position relatively by +1.
+        // W changes the x position relatively by -1.
+        // 2 - dir will correctly map 1 ~> 1 and 3 ~> -1.
+        ant->current_position.x += 2 - ant->direction;
     } else {
-        ant->current_position.y -= ant->direction - 1;
+        // direction is either N or S (0 or 2).
+        // N changes the y position relatively by -1.
+        // S changes the y position relatively by +1.
+        // dir - 1 will correctly map 0 ~> -1 and 2 -> 1.
+        ant->current_position.y += ant->direction - 1;
     }
 }
 
@@ -152,15 +161,11 @@ char validate_ant_pos(Ant* ant) {
 
 int main(int argc, char *argv[])
 {
-    start_encode(BOARD_LENGTH, BOARD_LENGTH, 50);
+    start_encode(BOARD_LENGTH, BOARD_LENGTH, 5);
     Ant* ant = init_ant();
     while(validate_ant_pos(ant)) {
         char pos_val = buffer[ant->current_position.y][ant->current_position.x];
-        if(!pos_val) {
-            rotate_ant_cw(ant, 1);
-        } else {
-            rotate_ant_ccw(ant, 1);
-        }
+        rotate_ant(ant, 1, pos_val == 1 ? -1 : 1);
         buffer[ant->current_position.y][ant->current_position.x] = !pos_val;
         move_ant_forward(ant);
         next_frame((char*) buffer);
