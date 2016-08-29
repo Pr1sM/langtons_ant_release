8be74df282e5f20f260dce4d3e19dfe6766b05bb
Srinivas Dhanwada <dhanwads@gmail.com>
2016-08-28 20:01:58 -0500

    Added cylinder mode Removed binary (not necessary)

--- ant/ant.c
+++ ant/ant.c
@@ -8,6 +8,7 @@
 
 #include <stdlib.h>
 #include "ant.h"
+#include "../parser/parser.h"
 
 Ant* init_ant(int x_size, int y_size) {
     // allocate memory for the ant and set the
@@ -31,7 +32,7 @@ void rotate_ant(Ant* ant, int num_times, int dir) {
     }
 }
 
-void move_ant_forward(Ant* ant) {
+void move_ant_forward(Ant* ant, int x_size, int y_size) {
     // The cardinal directions N, E, S, W are represented with values 0, 1, 2, 3, respectively.
     // That means a value of 0 or 2 affects the y direction, and 1 or 3 affects the x direction.
     // Check which axis we need to change first.
@@ -41,18 +42,25 @@ void move_ant_forward(Ant* ant) {
         // W changes the x position relatively by -1.
         // 2 - dir will correctly map 1 ~> 1 and 3 ~> -1.
         ant->current_position.x += 2 - ant->direction;
+        
+        // Check if we are in cylinder mode and apply a wrap
+        // if the is out of bounds in the x axis
+        if(BORDER_MODE > 0) {
+            ant->current_position.x += ant->current_position.x <  0      ?  x_size :
+                                       ant->current_position.x >= x_size ? -x_size : 0;
+        }
     } else {
         // direction is either N or S (0 or 2).
         // N changes the y position relatively by -1.
         // S changes the y position relatively by +1.
-        // dir - 1 will correctly map 0 ~> -1 and 2 -> 1.
+        // dir - 1 will correctly map 0 ~> -1 and 2 ~> 1.
         ant->current_position.y += ant->direction - 1;
     }
 }
 
 char validate_ant_pos(Ant* ant, int x_size, int y_size) {
-    return ant->current_position.x >= 0 &&
-           ant->current_position.x < x_size &&
+    int x_pass = BORDER_MODE > 0 || (ant->current_position.x >= 0 && ant->current_position.x < x_size);
+    return x_pass &&
            ant->current_position.y >= 0 &&
            ant->current_position.y < y_size;
 }
